e5f141a28bf077769d1e9dd50648ae7607087aef
Vivek <vkdhiman93@gmail.com>
2022-02-15 17:22:13 +0530

    first commit

---
+++ run.py
@@ -0,0 +1,7 @@
+from app import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+# __init__.py is what you use to bring the files together in the app and tell the python interpreter it is a package.
